fd23c0538794a7e0ebb71765e9aa0c3b0e684419
yama-natuki <yama.natuki@gmail.com>
2012-08-22 14:56:23 +0900

    offset reset.

--- temper.c
+++ temper.c
@@ -9,8 +9,8 @@
 
 /* Calibration adjustments */
 /* See http://www.pitt-pladdy.com/blog/_20110824-191017_0100_TEMPer_under_Linux_perl_with_Cacti/ */
-static float scale = 1.0287;
-static float offset = -0.85;
+static float scale = 1;
+static float offset = 0;
 
 int main(){
 	int passes = 0;
